4fd684316711d3a19d2930e156ce0335e44ee6c0
David <destrada2193@gmail.com>
2021-09-16 16:05:12 -0700

    Complete components for Header, Meals, and MealsSummary

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import styles from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>Dave's Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="A table full of yummy food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
