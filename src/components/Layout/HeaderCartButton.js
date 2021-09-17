a5ed6f798b98ca4325449feec3a41b7a17fe9e2f
David <destrada2193@gmail.com>
2021-09-16 19:49:29 -0700

    Implement store/CartProvider logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,27 @@
+import { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const numOfCartItems = cartContext.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+  // by using array.reduce(), we can extract a single value for an array by 
+  // selecting the total number of items where each cart item is just a single
+  // aggregate of its total number of pieces of that item 
+
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
-          <CartIcon />
+        <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-          3
-      </span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,27 @@
-const CartProvider = props => {
+import { useReducer } from "react";
+import CartContext from "./cart-context";
 
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {
+
+  };
+
+  const removeItemFromCartHandler = (id) => {
+
+  };
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
