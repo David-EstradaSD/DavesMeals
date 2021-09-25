69b0658f4a42aa2fc42ec558d32822e35fa59e0a
David <destrada2193@gmail.com>
2021-09-25 16:47:43 -0700

    Handle errors logic in fetching data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,13 +6,21 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // we want to start this componenet with rendering data from the DB, thus we want to show this
+  const [httpError, setHttpError] = useState(null);
 
-  useEffect(() => { // This useEffect will only run the 1st time the component is rendered
+  useEffect(() => { // This will only run the 1st time the component is rendered
     const fetchMeals = async () => {
       const response = await fetch('https://react-udemy-http-fd441-default-rtdb.firebaseio.com/meals.json').then();
+
+
+      if (response.status !== 200) {
+        throw new Error('Something went wrong!');
+        // by instantiating a new Error object, we give it default properties such as "message"
+      }
+
       const responseData = await response.json();
       
-  // we need to transform our data "object" into an array of "meals"
+  // ---- we need to transform our data "object" into an array of "meals"
       const loadedMeals = [];
 
       for (const key in responseData) {
@@ -27,8 +35,11 @@ const AvailableMeals = () => {
       setMeals(loadedMeals); // update our empty array with the fetched data from the DB
       setIsLoading(false); // done loading data from the DB
     };
-
-    fetchMeals();
+ 
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message); // we can invoke the built-in "message" property
+      }); // since fetchMeals() is an "async" function, it returns a "promise" and thus we can invoke the "catch" method directly (instead of wrapping this in a try catch block)
   }, []);
 
   if (isLoading) {
@@ -37,6 +48,14 @@ const AvailableMeals = () => {
         <p> Loading...</p>
       </section>
     );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
   }
 
   const mealsList = meals.map((meal) => (
